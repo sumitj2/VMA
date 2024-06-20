7e2bd5c5cbf9c93add3e2abf7e29f5833453259e
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-06-21 02:35:58 +0530

    Commit changes

--- BusinessLogic.VMA/VenderPaymentNotesBusinesslogic.cs
+++ BusinessLogic.VMA/VenderPaymentNotesBusinesslogic.cs
@@ -1,11 +1,5 @@
 ﻿using BusinessLogic.Abstraction.VMA.Contract;
 using Database.Abstraction.VMA.Contract;
-using Database.VMA.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Database.VMA.Repositories
 {
