ae217da370a609f0fbf0034738b3bf0ded38a895
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-06-21 02:09:44 +0530

    Adding blank add functionality View and View Models

---
+++ VMA/MVVM/Views/Add/AddPaymentsView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VMA.MVVM.Views.Add
+{
+    /// <summary>
+    /// Interaction logic for AddPaymentsView.xaml
+    /// </summary>
+    public partial class AddPaymentsView : UserControl
+    {
+        public AddPaymentsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
