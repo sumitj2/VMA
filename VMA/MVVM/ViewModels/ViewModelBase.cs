39a6852f0f1b90f53a9e6f8dff03b24de7707369
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-07-01 20:54:51 +0530

    Added custom validation

--- VMA/MVVM/ViewModels/ViewModelBase.cs
+++ VMA/MVVM/ViewModels/ViewModelBase.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VMA.MVVM.ViewModels
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public class ViewModelBase : IDataErrorInfo, INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -15,5 +16,21 @@ namespace VMA.MVVM.ViewModels
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        public string? this[string columnName]
+        {
+            get
+            {
+                var validationResults = new List<ValidationResult>();
+                var context = new ValidationContext(this) { MemberName = columnName };
+                Validator.TryValidateProperty(
+                    GetType().GetProperty(columnName)?.GetValue(this),
+                    context,
+                    validationResults);
+
+                return validationResults.Any() ? validationResults.First().ErrorMessage : string.Empty;
+            }
+        }
+        public string Error => null;
     }
 }
