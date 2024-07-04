c0e819377db44196f1a36ee383c67776c0d16112
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-07-05 00:14:15 +0530

    added logic to view tab on radio button click added logic for tds button to view disable tds check box added logic for add branch detaills on radio button click

--- VMA/MVVM/ViewModels/Add/AddPaymentsViewModel.cs
+++ VMA/MVVM/ViewModels/Add/AddPaymentsViewModel.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 using VMA.MVVM.ViewModels.Menus;
 
@@ -12,6 +13,79 @@ namespace VMA.MVVM.ViewModels.Add
     public class AddPaymentsViewModel : ViewModelBase
     {
         private readonly PaymentsViewModel _paymentViewModel;
+        private bool isGSTDetailsVisible;
+
+        public bool IsGSTDetailsVisible
+        {
+            get { return isGSTDetailsVisible; }
+            set 
+            {
+                if (isGSTDetailsVisible != value)
+                {
+                    isGSTDetailsVisible = value;                    
+                    OnPropertyChanged(nameof(GSTTabVisible));
+                }
+            }
+        }
+
+        private bool isTDSTextBoxVisible;
+
+        public bool IsTDSTextBoxVisible
+        {
+            get { return isTDSTextBoxVisible; }
+            set 
+            {
+                if(isTDSTextBoxVisible!=value)
+                {
+                    isTDSTextBoxVisible = value;
+                    OnPropertyChanged(nameof(TextBoxVisibility));
+                    OnPropertyChanged(nameof(TextBlockVisibility));
+
+                }
+            }
+        }
+
+        private bool isBranchNameVisible;
+
+        public bool IsBranchNameVisible
+        {
+            get { return isBranchNameVisible; }
+            set 
+            { 
+                
+                if (isBranchNameVisible != value)
+                {
+                    isBranchNameVisible = value;
+                    OnPropertyChanged(nameof(TextBoxBranchNameVisibility));
+                    OnPropertyChanged(nameof(TextBlockBranchNameVisibility));
+
+                }
+            }
+        }
+
+
+        public Visibility GSTTabVisible
+        {
+            get { return IsGSTDetailsVisible ? Visibility.Visible : Visibility.Collapsed; }
+        }
+        public Visibility TextBoxVisibility
+        {
+            get { return IsTDSTextBoxVisible ? Visibility.Visible : Visibility.Collapsed; }
+        }
+        public Visibility TextBlockVisibility
+        {
+            get { return IsTDSTextBoxVisible ? Visibility.Visible : Visibility.Collapsed; }
+        }
+
+        public Visibility TextBoxBranchNameVisibility
+        {
+            get { return IsBranchNameVisible ? Visibility.Visible : Visibility.Collapsed; }
+        }
+        public Visibility TextBlockBranchNameVisibility
+        {
+            get { return IsBranchNameVisible ? Visibility.Visible : Visibility.Collapsed; }
+        }
+
         public ICommand HidePaymentFormCommand { get; }
         public AddPaymentsViewModel(PaymentsViewModel vendorViewModel)
         {
