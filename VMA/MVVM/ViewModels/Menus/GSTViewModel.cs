7fd530e2aebe8f97282354229c2d3e2c003dc23a
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-07-14 22:54:07 +0530

    Get GST details and render on view

--- VMA/MVVM/ViewModels/Menus/GSTViewModel.cs
+++ VMA/MVVM/ViewModels/Menus/GSTViewModel.cs
@@ -2,8 +2,11 @@
 using BusinessLogic.Abstraction.VMA.Models;
 using Database.VMA.Repositories;
 using System;
+using System.Buffers;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -15,6 +18,7 @@ namespace VMA.MVVM.ViewModels.Menus
     {
         private readonly MainViewModel _parentViewModel;
         private readonly IGstcalculationMasterBusinessLogic _gstcalculationMasterBusinessLogic;
+
         public GSTViewModel(IGstcalculationMasterBusinessLogic gstcalculationMasterBusinessLogic, MainViewModel parentViewModel)
         {
             _parentViewModel = parentViewModel;
@@ -22,7 +26,76 @@ namespace VMA.MVVM.ViewModels.Menus
             AddShowGSTFormCommand = new ViewModelAsyncCommand<GstcalculationMasterModel>(ShowGSTForm);
             HideGSTFormCommand = new ViewModelAsyncCommand<GstcalculationMasterModel>(HideGSTForm);
             EditGSTCommand = new ViewModelAsyncCommand<GstcalculationMasterModel>(EditGST);
+            _ = GetGSTMaster();
         }
+        private SearchModel _selectComboItem;
+        public SearchModel SelectComboItem
+        {
+            get { return _selectComboItem; }
+            set { _selectComboItem = value; }
+        }
+
+        private GstcalculationMasterModel _selectedVendor;
+        public GstcalculationMasterModel SelectedVendor
+        {
+            get { return _selectedVendor; }
+            set { _selectedVendor = value; OnPropertyChanged(nameof(SelectedVendor)); }
+        }
+
+        private string _searchValue;
+        public string SearchValue
+        {
+            get { return _searchValue; }
+            set
+            {
+                _searchValue = value;
+
+                if (SelectComboItem != null && !string.IsNullOrEmpty(value))
+                {
+                    PropertyInfo? propertyInfo = typeof(GstcalculationMasterModel)?.GetProperty(SelectComboItem.NameSearch.Replace(" ", ""));
+
+                    GSTMaster = new ObservableCollection<GstcalculationMasterModel>(TempGSTMaster.Where(x => propertyInfo?.GetValue(x, null)?.ToString()?.ToLower(System.Globalization.CultureInfo.CurrentCulture).Contains(value, StringComparison.CurrentCultureIgnoreCase) ?? false));
+                }
+                else
+                {
+                    GSTMaster = TempGSTMaster;
+                }
+
+                OnPropertyChanged(nameof(SearchValue));
+            }
+        }
+
+        #region Observable collections
+        private ObservableCollection<GstcalculationMasterModel> _vendors;
+        private ObservableCollection<GstcalculationMasterModel> _tempvendors;
+        private ObservableCollection<SearchModel> _comboItem;
+
+        public ObservableCollection<GstcalculationMasterModel> GSTMaster
+        {
+            get { return _vendors; }
+            set
+            {
+                _vendors = value;
+                OnPropertyChanged(nameof(GSTMaster));
+            }
+        }
+
+        public ObservableCollection<GstcalculationMasterModel> TempGSTMaster
+        {
+            get { return _tempvendors; }
+            set
+            {
+                _tempvendors = value;
+                OnPropertyChanged(nameof(TempGSTMaster));
+            }
+        }
+
+        public ObservableCollection<SearchModel> ComboItem
+        {
+            get { return _comboItem; }
+            set { _comboItem = value; }
+        }
+        #endregion
 
         private Task ShowGSTForm(GstcalculationMasterModel model)
         {
@@ -32,7 +105,7 @@ namespace VMA.MVVM.ViewModels.Menus
 
         private async Task EditGST(GstcalculationMasterModel model)
         {
-            throw new NotImplementedException();
+            
         }
 
         public Task HideGSTForm(object model)
@@ -40,8 +113,11 @@ namespace VMA.MVVM.ViewModels.Menus
             _parentViewModel.CurrentChildView = this;
             return Task.CompletedTask;
         }
-
-        
+        private async Task GetGSTMaster()
+        {
+            var vendors = await _gstcalculationMasterBusinessLogic.GetAllGstMaster().ConfigureAwait(true);
+            GSTMaster = TempGSTMaster = new ObservableCollection<GstcalculationMasterModel>(vendors);
+        }
 
         #region commands
 
