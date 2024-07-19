f9924e34a94c49b278c6821225ecbcc6909c988e
pbobade <pbobade@citi-us.com>
2024-07-19 17:43:36 +0530

    Making changes to unselect listview item.

--- VMA/MVVM/Views/Menus/SettingsView.xaml.cs
+++ VMA/MVVM/Views/Menus/SettingsView.xaml.cs
@@ -24,5 +24,10 @@ namespace VMA.MVVM.Views
         {
             InitializeComponent();
         }
+
+        private void GroupBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            itemsListBox.UnselectAll();
+        }
     }
 }
