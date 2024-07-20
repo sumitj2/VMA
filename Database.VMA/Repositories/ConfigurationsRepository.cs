5de732ac0436be411232f66e4350ddea4faa3b8c
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-07-20 17:59:22 +0530

    Added configuration repository

---
+++ Database.VMA/Repositories/ConfigurationsRepository.cs
@@ -0,0 +1,46 @@
+﻿using Database.Abstraction.VMA.Contract;
+using Database.VMA.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.VMA.Repositories
+{
+    public class ConfigurationsRepository : IConfigurationsRepository
+    {
+        private readonly VendorManagementDbContext _context;
+        public ConfigurationsRepository(VendorManagementDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddConfiguration(Configuration ConfigurationEntity)
+        {
+            await _context.AddAsync(ConfigurationEntity).ConfigureAwait(true);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateDeleteConfigurations(Configuration ConfigurationEntity)
+        {
+            var existingEntity = _context.Configurations.Find(ConfigurationEntity.Id);
+            if (existingEntity == null)
+            {
+                _context.Attach(ConfigurationEntity);
+            }
+            else
+            {
+                _context.Entry(existingEntity).CurrentValues.SetValues(ConfigurationEntity);
+            }
+            await _context.SaveChangesAsync();
+        }
+        public async Task<IEnumerable<Configuration>> GetAllConfigurations()
+        {
+            return await _context.Configurations.ToListAsync();
+        }
+        public async Task<Configuration?> GetConfigurationByKey(string CFGKey)
+        {
+            return await _context.Configurations.Where(x => x.Cfgkey == CFGKey).FirstOrDefaultAsync();
+        }
+    }
+}
