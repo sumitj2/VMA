e591be5c5058743d7453c52cf4b5ebbf9b5208cc
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-07-30 00:36:38 +0530

    Yearly report commit 1. Query 2.UI Chnages

---
+++ BusinessLogic.Abstraction.VMA/Contract/IYearlyMonthlyReportPDF.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Abstraction.VMA.Contract
+{
+    public interface IYearlyMonthlyReportPDF
+    {
+        public Task GenerateYearlyReport(string? financilaYear, string? path);
+        public Task GenerateMonthlyReport(string? financilaYear,string? Month, string? path);
+    }
+}
