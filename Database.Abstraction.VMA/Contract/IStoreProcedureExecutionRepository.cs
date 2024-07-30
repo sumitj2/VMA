d40e055a3909a82de727b6b344724f825c838e90
sumitj2 <jagtapsumit.jagtap93@gmail.com>
2024-07-31 01:06:38 +0530

    Create reository for Store Procedure execution

---
+++ Database.Abstraction.VMA/Contract/IStoreProcedureExecutionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Database.VMA.Entities.CustomEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Abstraction.VMA.Contract
+{
+    public interface IStoreProcedureExecutionRepository
+    {
+        Task<List<YearlyReportData>> GetVendorServiceDataAsync(string detailsYear);
+    }
+}
